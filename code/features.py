07997772541889b7c5325f544c9f730035bdd390
Kanya Paramita <kanyaparamita@yahoo.com>
2020-03-10 13:26:44 +0100

    Resolve conflict

--- code/features.py
+++ code/features.py
@@ -1,5 +1,6 @@
 import nltk
 import spacy
+from pycorenlp import StanfordCoreNLP
 
 # clickbaits tend to have longer headlines
 def number_of_words(sentence):
@@ -47,30 +48,48 @@ def has_comparative(sentence):
             return True
     return False
 
-
 def has_superlative(sentence):
     for token in sentence:
         if token.tag_ == 'JJS' or token.tag_ == 'RBS':
             return True
     return False
 
+def sentiment_score(sentence):
+    snlp = StanfordCoreNLP('http://localhost:9000')
+    result = snlp.annotate(sentence,
+        properties={
+            'annotators': 'sentiment',
+            'outputFormat': 'json',
+            'timeout': 1000,
+        })
+    return result
+
 
 if __name__ == '__main__':
-    #nlp = spacy.load("en_core_web_sm")
-    nlp = spacy.load("en_core_web_lg")
+    #nlp = spacy.load("en_core_web_lg")
+    nlp = spacy.load("en_core_web_sm")
+    sentencex = "A Couple Did A Stunning Photo Shoot With Their Baby After Learning She Had An Inoperable Brain Tumor. People Don't Know Who David Bowie Is And We're Here To Clear That Up. 24 Times Ruby Rose And Phoebe Dahl Defined Relationship Goals In 2015. G.M. Lowers 2009 Outlook for All U.S. Auto Sales. Your dog is not listening to me"
     sentence0 = "A Couple Did A Stunning Photo Shoot With Their Baby After Learning She Had An Inoperable Brain Tumor"
-    sentence1 = '31 Posts That Prove "Harry Potter" Has The Funniest Fans'
-    sentence2 = "15 Chocolate Desserts To Console You This Valentine's Day"
+    sentence1 = "People Don't Know Who David Bowie Is And We're Here To Clear That Up"
+    sentence2 = "24 Times Ruby Rose And Phoebe Dahl Defined Relationship Goals In 2015"
     sentence3 = "G.M. Lowers 2009 Outlook for All U.S. Auto Sales"
     sentence4 = "Your dog is not listening to me"
-    sentence1 = sentence1.lower()
-    doc = nlp(sentence1)
-    # print(doc)
-    # print(number_of_words(doc))
-    # print(avg_length_of_word(doc))
-    # print(stopword_percentage(doc))
-    # print(has_number(doc))
-    # print(has_determiner(doc))
+
+    doc = nlp(sentence4)
+    results = sentiment_score(sentence0)
+
+    for sentence in results["sentences"]:
+        print("{}: {}".format(
+            " ".join([t["word"] for t in sentence["tokens"]]),
+            sentence["sentimentValue"]
+            )
+        )
+    #print(doc)
+    #print(number_of_words(doc))
+    #print(avg_length_of_word(doc))
+    #print(stopword_percentage(doc))
+    #print(has_number(doc))
+    #print(has_determiner(doc))
     for token in doc:
         print(token.text, token.tag_)
 
@@ -78,7 +97,3 @@ if __name__ == '__main__':
 
     sentence2 = sentence2.lower()
     doc = nlp(sentence2)
-    for token in doc:
-        print(token.text, token.tag_)
-
-    print(has_superlative(doc))
